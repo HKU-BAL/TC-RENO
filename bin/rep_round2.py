9518ee0fe39f2a396740342c280e0f07509f1eed
qhli1 <44329221+qhli1@users.noreply.github.com>
2021-07-23 20:33:43 +0800

    Update rep_round2.py

--- bin/rep_round2.py
+++ bin/rep_round2.py
@@ -17,14 +17,7 @@ for line in f.readlines():
            round_subreads_rep[line[n]].add(line[0])
     round_rep_subreads[line[0]].add(line[0])
 f.close()
-'''
-f = open(sys.argv[1]+'/rep_round1.bed','r')
-for line in f.readlines():
-    info = line.strip()
-    line = line.strip().split('\t')
-    read_info[line[3]] = info + '\n'
-f.close()
-'''
+
 path = sys.argv[1]+'/cluster_round2_TC_RENO/'
 path1 = sys.argv[1]+ '/annotation_round2/'
 path3 = sys.argv[1]+ '/annotation_round3/'
@@ -34,23 +27,25 @@ try:
 except OSError:
     pass
 
-
+file_name = {}
 files = os.listdir(path)
 for file in files:
+    
     f = open(path1 + file, 'r')
     for line in f.readlines():
         info = line
         line = line.strip().split('\t')
         read_info[line[3]] = info 
     f.close()
-
+    
     f = open(path + file ,'r')
     f_write = open(path3 + file,'w')
     for line in f.readlines():
         line = line.strip().split('\t')
         rep = line[3]
+        file_name[rep] = file
         names = line[12].split('|')[0].split(',')
-        f_write.write(read_info[line[3]] )
+    
         for name in names:
             if len(name)>3:
                round_rep_subreads[rep].add(name)
@@ -58,8 +53,7 @@ for file in files:
         round_rep_subreads[rep].add(rep)
     f.close()
     f_write.close()
-
-
+   
 
 remove = set()
 for rep in round_rep_subreads:
@@ -77,29 +71,36 @@ for rep in round_rep_subreads:
        for i in round_rep_subreads[rep]:
            reads.append(i)
 read_num = Counter(reads)  
+cutoff = float(sys.argv[2])
 
 path2 = sys.argv[1]
-#f_write = open(path2 + 'rep_round2.bed','w')
+
 f_write1 = open(path2 + '/rep_round2.support.bed','w')
-#f_write_short = open(path2 + 'rep_round2.short.bed','w')
+
+print len(round_rep_subreads)
 for rep in round_rep_subreads:
-    if rep not in remove:
+    if rep not in remove and len(rep) > 0:
        number = 0
        for i in round_rep_subreads[rep]:
             number += 1.0/read_num[i]
-#       print rep, number
-      # if number >= cutoff:
- #      f_write.write(read_info[rep])
-      # information = read_info[rep].split('\t')
-    #      f_write_short.write(information[0] + '\t' + information[1] + '\t' + information[2] + '\t' + information[3] + '\t' + information[4] + '\t' + information[5] +'\n')
+
        info = ''
        for i in round_rep_subreads[rep]:
            if i != rep:
               info += '\t' + i
+       if number >= cutoff:
+
+
+          file = file_name[rep]
+          f_write = open(path3 + file,'a')
+          f_write.write(read_info[rep]) 
+          f_write.close()
+
        f_write1.write(rep + info + "|" + str(number) + '\n' )
-#f_write.close()
+
 f_write1.close()
-#f_write_short.close()
+
+
 
 
 
